82a9c6394124690b5c9dbf07d1029306dd3c6ace
Rommel110207 <rommelmg@uamv.edu.ni>
2025-05-26 22:07:13 -0600

    Maximo e minimo

---
+++ maximo_minimo.py
@@ -0,0 +1,6 @@
+#A partir de una lista de números reales, encuentra el mayor y el menor valor. 
+numeros = [12.5, 45.3, 67.8, 23.1, 89.0, 34.6, 56.2, 78.9, 90.5, 11.4]
+mayor = max(numeros)
+menor = min(numeros)
+print(f"El mayor valor es: {mayor}")
+print(f"El menor valor es: {menor}")
